38867a82be8000338c89015ee41ad63f62d8cd85
Jirapat Jantsakool <jirapat.jantsakool@gmail.com>
2016-02-21 10:15:24 +0700

    example controller

---
+++ src/main/java/com/kmutt/stcp/web/TestController.java
@@ -0,0 +1,33 @@
+package com.kmutt.stcp.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+/**
+ * Created by jirapatj on 2/21/16.
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    private final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String index(Map<String, Object> model) {
+
+        logger.debug("index() is executed!");
+
+        model.put("title", "test");
+        model.put("msg", "message test test test");
+
+        return "test";
+    }
+
+
+}
