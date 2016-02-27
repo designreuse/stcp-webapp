927edf52a4ec42d757a66be637b83b38ab6ce051
unknown <admin@SIL-SD3-014nb.CWNSI.TH>
2016-02-27 18:38:12 +0700

    Course Plan Entity & Sample Data

---
+++ src/main/java/com/kmutt/stcp/entity/CoursePlanEntity.java
@@ -0,0 +1,29 @@
+package com.kmutt.stcp.entity;
+
+import java.util.List;
+
+public class CoursePlanEntity {
+
+	//Field//
+	private int studentId;
+	private List<CoursePlanSemeterEntity> semeterPlanList;
+	
+		
+	//Property//
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	
+	public List<CoursePlanSemeterEntity> getSemeterPlanList() {
+		return semeterPlanList;
+	}
+	public void setSemeterPlanList(List<CoursePlanSemeterEntity> semeterPlanList) {
+		this.semeterPlanList = semeterPlanList;
+	}
+	
+	//Method//
+	
+}
