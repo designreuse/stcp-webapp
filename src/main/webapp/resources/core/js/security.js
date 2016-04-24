c424c9f3b0642316313477498e9397ee9f513a6c
joecoolll <joecooll.jb@gmail.com>
2016-04-24 16:59:15 +0700

    login v3

--- src/main/webapp/resources/core/js/security.js
+++ src/main/webapp/resources/core/js/security.js
@@ -10,11 +10,13 @@ $(document).ready(function() {
             type: "GET",
             url: "http://localhost:8080/stcp/login",
             dataType: "json",
-            data: { textsearch: "" },
+            data: { textsearch: "testinput" },
             success: function (data) {
             	
             	if(data != null){
-            		alert("Pass");
+            		data.forEach(function (subject) {
+            			alert(subject.detailThai);
+            		});
             	} 
             	else{
             		alert("Fale");
@@ -22,6 +24,37 @@ $(document).ready(function() {
             },
             error: function (xml, status, errMsg) {
             	alert(xml);
+            	alert(status);
+            	alert(errMsg);
+            }
+        });
+	});
+	
+	$("#btnRegister").on('click', function(e){
+		e.preventDefault();
+		
+		var txtEmail = $('#tbxRegisterEmail').val();
+		
+		alert(txtEmail);
+		
+		$.ajax({
+            type: "GET",
+            url: "http://localhost:8080/stcp/SentMailConfirm",
+            dataType: "json",
+            data: { email: txtEmail },
+            success: function (data) {
+            	
+            	if(data != null){
+            		data.forEach(function (account) {
+            			alert(account.username);
+            		});
+            	} 
+            	else{
+            		alert("Fale");
+            	}
+            },
+            error: function (xml, status, errMsg) {
+            	//alert(xml);
             	alert(status);
             	alert(errMsg);
             }
